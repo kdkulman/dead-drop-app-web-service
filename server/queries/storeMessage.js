c37850de5b7ff658f82e975d3bf5f4be0abb9378
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2022-11-18 15:18:31 -0800

    Update storeMessage.js

--- server/queries/storeMessage.js
+++ server/queries/storeMessage.js
@@ -1,5 +1,5 @@
-const { response } = require('express')
-const { request } = require('express')
+// const { response } = require('express')
+// const { request } = require('express')
 const express = require('express')
 const pool = require('../utilities/databaseConnection.js')
 let router = express.Router() 
@@ -7,26 +7,21 @@ let router = express.Router()
 router.post("/", (request, response) => {
 
     //THIS IS NOT FINAL, AMEND!!!!!!! 
-    const text = 'banana';//request.text;
-    // const country = request.country;
+    //let messageId = results[0].Id + 1;
+    const text = request.text; //request.text;
+    const country = request.country;
     const dateCreated = "CURRENT_TIMESTAMP";
     const sender = "null";
     const recipient = "null";
     const messageLength = text.length;
-    const isRead = 0;
-    const tempMessageId = 100;
-    const tempText = 'banana';
-    let messageId = 0;
 
-    let idQuery = 'SELECT MAX(MessageId) AS Id FROM Messages'
-
-    pool.query(idQuery, function(err, results, fields) {
+    pool.query('SELECT MAX(MessageId) AS Id FROM Messages', function(err, results, fields) {
         if (err) {
             console.log(err);
             console.log("this activates");
 
         } else {
-            messageId = results[0].Id + 1;
+            let messageId = results[0].Id + 1;
             let theQuery = `INSERT INTO Messages(MessageId, Text, DateCreated, MessageLength) VALUES(${messageId}, '${text}', CURRENT_TIMESTAMP, ${messageLength})`
             //let values = [tempMessageId, 'testbanana', 10]
         
@@ -38,33 +33,12 @@ router.post("/", (request, response) => {
                     })
                 } else {
                     response.status(200).send({
-                        message: 'Inserted Message' 
+                        message: 'Inserted Message, ID: ' + messageId 
                     })
                 }
             });
-            console.log(results[0].Id); //print 666
         }
     });
-
-    console.log(messageId + 1);
-
-
-
-    // pool.promise().query(theQuery, values) 
-    //     .then(result => {
-    //         console.log("Inserted message");
-    //         response.status(200).send({
-    //             message: 'Inserted Message' 
-    //         })
-    //     })
-        
-        // .catch(error => {
-        //     console.log(error);
-        //     console.log("error, message could not be stored");
-        //     response.status(401).send({
-        //         message: 'Could not store message in database' 
-        //     })
-        // })
 });
 
 module.exports = router
