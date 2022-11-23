57212d075a6e2240fe3953649e820a4d59e10f34
Stephen VanLuven <101437287+Ledagra@users.noreply.github.com>
2022-11-23 13:58:30 -0800

    added user creation

---
+++ server/queries/storeUser.js
@@ -0,0 +1,29 @@
+// const { response } = require('express')
+// const { request } = require('express')
+const express = require('express')
+const bodyParser = require('body-parser');
+const pool = require('../utilities/databaseConnection.js')
+let router = express.Router() 
+router.use(bodyParser());
+
+router.post("/", (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    const userName = request.body.username;
+    const passWord = request.body.password;
+    const nickName = request.body.nickname;
+
+    pool.query(`INSERT INTO USERS(Username, Password, Nickname) VALUES('${userName}', '${passWord}', '${nickName}')`, function(err, results, fields) {
+        if (err) {
+            console.log(err);
+        } else {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.status(200).send({
+                userName: 'Created user, username: ' + userName,
+            })
+        }
+    });
+});
+
+module.exports = router
