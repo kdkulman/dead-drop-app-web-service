7ffa36ba603558c96704672e827c544f1e2fb91a
Nicholas Zhuk <nicholaszhuk@gmail.com>
2022-11-29 18:37:41 -0800

    getFriends made, shows da list of friends

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ app.use('/deleteMessage', require('./queries/deleteMessage.js'));
 app.use('/getMessage', require('./queries/getMessage.js'));
 app.use('/storeUser', require('./queries/storeUser.js'));
 app.use('/addFriend', require('./queries/addFriend.js'));
+app.use('/getFriends', require('./queries/getFriends.js'));
 
 app.listen(port, () => {
   console.log("Server up and running on port: " + (port));
