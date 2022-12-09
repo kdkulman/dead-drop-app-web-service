00ed678ca88d6f09ec03a2294a9f0837bc9314cb
Ledagra <svanluven90@gmail.com>
2022-12-08 23:16:11 -0800

    Create sendFriendRequest.js

---
+++ server/queries/sendFriendRequest.js
@@ -0,0 +1,31 @@
+// const { response } = require('express')
+// const { request } = require('express')
+const express = require('express')
+const bodyParser = require('body-parser');
+const pool = require('../utilities/databaseConnection.js')
+let router = express.Router() 
+router.use(bodyParser());
+
+router.post("/", (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    const userName = request.body.username;
+    const passWord = request.body.password;
+    const nickName = request.body.nickname;
+    let theQuery = 
+    `INSERT INTO USERS(Username, Password, Nickname) 
+    VALUES('${userName}', '${passWord}', '${nickName}')`
+    pool.query(theQuery, function(err, results, fields) {
+        if (err) {
+            console.log(err);
+        } else {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.status(200).send({
+                userName: 'Created user, username: ' + userName,
+            })
+        }
+    });
+});
+
+module.exports = router
